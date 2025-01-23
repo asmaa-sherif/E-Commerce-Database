f6cd51f9704f5588d6b6e970fa9e8c14d2830baa
Asmaa Sherif Alsaber <64560423+asmaa-sherif@users.noreply.github.com>
2025-01-23 15:24:30 +0000

    Update recommend-products.sql

--- queries/recommend-products.sql
+++ queries/recommend-products.sql
@@ -1,1 +1,23 @@
-
+-- Can you design a query to suggest popular products in the same category for the same author, excluding the Purchsed product from the recommendations?
+SELECT p.product_id,
+      p.name
+  FROM product p
+  LEFT JOIN order_details od -- left join to get all products including the not purchased ones
+  ON p.product_id = od.product_id
+  WHERE p.category_id =
+      (SELECT p2.category_id
+       FROM product p2
+       WHERE product_id = '261') -- Get Category_id
+  AND p.author_id =
+      (SELECT p2.author_id
+       FROM product p2
+       WHERE product_id = '261') -- Get Author_id
+  AND p.product_id NOT IN
+      (SELECT od.product_id
+       FROM orders o
+       JOIN order_details od ON o.order_id = od.order_id
+       WHERE o.customer_id = '18')-- Get Purchased Products
+  GROUP BY p.product_id,
+           p.name
+  ORDER BY sum(od.quantity)DESC, sum(od.quantity*od.unit_price)
+  LIMIT 5;
