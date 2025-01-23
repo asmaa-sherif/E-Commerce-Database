b05364e6367a49c8b8f2765f059bc121618661e2
Asmaa Sherif Alsaber <64560423+asmaa-sherif@users.noreply.github.com>
2025-01-23 15:45:14 +0000

    Update lock-product-row.sql

--- queries/lock-product-row.sql
+++ queries/lock-product-row.sql
@@ -1,1 +1,12 @@
+-- Lock the Entire Row for Product ID = 211
+START TRANSACTION;
+
+-- Lock the entire row
+SELECT *
+FROM Product
+WHERE ProductID = 211
+FOR UPDATE;
+
+-- No updates can be made to the row for ProductID = 211
+COMMIT;
 
