f7efda739c879ae2c67c63a02507e824981abc10
Asmaa Sherif Alsaber <64560423+asmaa-sherif@users.noreply.github.com>
2025-01-23 15:18:02 +0000

    Update search-products-camera.sql

--- queries/search-products-camera.sql
+++ queries/search-products-camera.sql
@@ -1,1 +1,13 @@
-
+-- Write a SQL query to search for all products with the word "camera" in either the product name or description.
+SELECT 
+    ProductID,
+    Name AS ProductName,
+    ShortDesc AS ShortDescription,
+    LongDesc AS LongDescription,
+    Price
+FROM 
+    Product
+WHERE 
+    LOWER(Name) LIKE '%camera%' OR
+    LOWER(ShortDesc) LIKE '%camera%' OR
+    LOWER(LongDesc) LIKE '%camera%';
