bd26ab5e7c38f1015de527d2a17e7fab99144c7e
Asmaa Sherif Alsaber <64560423+asmaa-sherif@users.noreply.github.com>
2025-01-23 15:45:39 +0000

    Update lock-product-quantity.sql

--- queries/lock-product-quantity.sql
+++ queries/lock-product-quantity.sql
@@ -1,1 +1,12 @@
+-- Lock the Field Quantity for Product ID = 211
+START TRANSACTION;
+
+-- Lock the specific field
+SELECT Quantity
+FROM Product
+WHERE ProductID = 211
+FOR UPDATE;
+
+-- No updates can be made to the Quantity field for ProductID = 211
+COMMIT;
 
